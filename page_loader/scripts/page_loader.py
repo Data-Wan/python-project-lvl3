797653438288d39e37a02b3172e3e2bac4d3a014
Data-Wan <ustimenko.work@gmail.com>
2021-04-11 15:24:25 +0300

    added logging for project

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 
 """Module with UI."""
+import logging
+
 from page_loader.cli import take_args
 from page_loader.page_loader import download
 
@@ -9,7 +11,18 @@ args = take_args()
 
 def main():
     """Print generate diff for 2 json files."""
+    # Add logger
+    logging.basicConfig(
+        filename='debug.log',  # noqa: WPS317
+        level=logging.DEBUG,
+        format='%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)',  # noqa: WPS323, E501
+        datefmt='%Y-%m-%d %H:%M:%S',  # noqa: WPS323
+    )
+    logger = logging.getLogger(__name__)
+
+    logger.info('start downloading webpage')
     print(download(args.url, args.path))
+    logger.info('finished downloading')
 
 
 if __name__ == '__main__':
